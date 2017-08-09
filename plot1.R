68bb6192f804d4c26a8257d3cfeff479a5c3a059
Jeff Metcalf <jeffrey_p_metcalf@uhc.com>
2017-08-08 21:21:12 -0400

    Add files via upload
    There are 9 files total.  4 plots, 4 R programs corresponding to the plots, and a README file.

---
+++ plot1.R
@@ -0,0 +1,27 @@
+#Set working directory to preferred location
+setwd("C:/Users/jmetca1/Desktop/Ad Hoc/Data Science Certification/Exploratory Analysis/Week1_Project")
+
+#Download data from the internet
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+              destfile="./exdata_data_household_power_consumption.zip")
+
+#Unzip the data from the downloaded zip file
+unzip("exdata_data_household_power_consumption.zip")
+
+#Read the data (reading all lines)
+epc<-read.table("household_power_consumption.txt", header =TRUE, sep=";", stringsAsFactors=FALSE)
+
+#Create the subset of data actually needed for the assignment
+epc_subset<-subset(epc, Date == "1/2/2007" | Date=="2/2/2007")
+
+#Load lubridate package so I can use it to build usable date/time fields
+library(lubridate)
+epc_subset$Date_Time<-with(epc_subset, dmy(Date) + hms(Time))
+
+#Set global parameter for number of plots per row, column.  Setting equal to 1
+par(mfrow=c(1,1))
+
+# plot 1
+png(filename="Plot_1.png", width = 480, height =480) 
+hist(as.numeric(epc_subset$Global_active_power),col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,31 @@
+#Set working directory to preferred location
+setwd("C:/Users/jmetca1/Desktop/Ad Hoc/Data Science Certification/Exploratory Analysis/Week1_Project")
+
+#Download data from the internet
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+              destfile="./exdata_data_household_power_consumption.zip")
+
+#Unzip the data from the downloaded zip file
+unzip("exdata_data_household_power_consumption.zip")
+
+#Read the data (reading all lines)
+epc<-read.table("household_power_consumption.txt", header =TRUE, sep=";", stringsAsFactors=FALSE)
+
+#Create the subset of data actually needed for the assignment
+epc_subset<-subset(epc, Date == "1/2/2007" | Date=="2/2/2007")
+
+#Load lubridate package so I can use it to build usable date/time fields
+library(lubridate)
+epc_subset$Date_Time<-with(epc_subset, dmy(Date) + hms(Time))
+
+#Set global parameter for number of plots per row, column.  Setting equal to 1
+par(mfrow=c(1,1))
+
+# plot 3
+png(filename ="Plot_3.png", width = 480, height =480)
+plot((epc_subset$Date_Time),as.numeric(epc_subset$Sub_metering_1), type="n", ylab ="Energy sub metering", xlab="")
+lines((epc_subset$Date_Time),as.numeric(epc_subset$Sub_metering_1), type = "l",col = "black")
+lines((epc_subset$Date_Time),as.numeric(epc_subset$Sub_metering_2), type = "l",col = "red")
+lines((epc_subset$Date_Time),as.numeric(epc_subset$Sub_metering_3), type = "l",col = "blue")
+legend("topright", col=c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1)
+dev.off()
